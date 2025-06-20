04d34e490c6542926964d361daf6537c188dd946
Hossam Osman <hossam.osmann97@gmail.com>
2025-06-20 22:15:03 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,7 @@
-console.log(C is fun);
-console.log(Python is cool);
-console.log(JavaScript is amazing);
+const Line1 = "C is fun"
+const Line2 = "Python is cool"
+const Line3 = "JavaScript is amazing"
+  console.log(Line1);
+  console.log(Line2);
+  console.log(Line3);
 
