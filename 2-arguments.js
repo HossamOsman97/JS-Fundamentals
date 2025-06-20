d20b275fc6118d82d70fe9d837f0c1d30b931e18
Hossam Osman <hossam.osmann97@gmail.com>
2025-06-20 23:31:42 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,9 @@
-const argCount = Process.argv.legnth-2;
-if {
-    (argCount === 0)
-        console.log("No argument");}
-else if { (argCount === 1)
-     console.log("Argument fount")}
-else { console.log("Argument fount")}
+const argCount = process.argv.length - 2;
+
+if (argCount === 0) {
+  console.log("No argument");
+} else if (argCount === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
