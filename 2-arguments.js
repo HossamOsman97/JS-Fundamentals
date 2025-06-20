21812f2d9574dc1a30728fbe9be32632dcbd8372
Hossam Osman <hossam.osmann97@gmail.com>
2025-06-20 23:07:28 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
 const args = ProcessingInstruction.args.slice(2);
-{
-    if (args.length === 0)
+if {
+    (args.length === 0)
         console.log("No argument");}
-{else if (args.length === 1)
+else if { (args.length === 1)
      console.log("Argument fount")}
-{else console.log("Argument fount")}
+else { console.log("Argument fount")}
